7261d8ab025a09bbe00b961129270c50eaf217bf
KIMSEI1124 <workju1124@gmail.com>
2023-02-21 18:12:58 +0900

    init: Music Player
    볼륨 조절
    노래 재생

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Music Player
+HTML5 + CSS3 + JS로 만들어보는 뮤직 플레이어
+
+# Reference
+## CSS
+- [How to Center a Div in 60 seconds!](https://youtube.com/shorts/tOctz4Fio3Q?feature=share)
+- [21 CSS Range Sliders](https://freefrontend.com/css-range-sliders/)
+- [CSS / 애니메이션 / transform / 회전,확대,축소,비틀기 등 형태 변형하는 속성](https://www.codingfactory.net/12593)
+- [San Francisco 웹 폰트](https://foodchain.tistory.com/163)
+- [웹폰트(@font-face) 적용법](https://webdir.tistory.com/56)
+
+## JS
+- [Javascript/자바스크립트 audio play (오디오 재생) 방법, Audio 객체 예시)](https://mjmjmj98.tistory.com/31)
+
+## Import
+- [fontawesome](https://fontawesome.com/search?o=r&m=free)
---
+++ script.js
@@ -0,0 +1,144 @@
+let now_playing = document.querySelector(".now-playing");
+let music_pic = document.querySelector(".music-pic");
+let music_name = document.querySelector(".music-name");
+let music_artist = document.querySelector(".artist");
+
+let playStop_btn = document.querySelector(".playStop-music");
+let pre_btn = document.querySelector(".pre-music");
+let next_btn = document.querySelector(".next-music");
+
+let music_slider = document.querySelector(".music-slider");
+let volume_slider = document.querySelector(".volume-slider");
+let current_time = document.querySelector(".current-time");
+let total_time = document.querySelector(".total-time");
+let this_music = document.createElement("audio");
+
+let music_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+const music_list = [
+  {
+    img: "On&On.jpg",
+    name: "On & On (feat. Daniel Levi)",
+    artist: "Cartoon",
+    music: "music/Cartoon - On & On (feat. Daniel Levi).mp3",
+  },
+  {
+    img: "MyHeart.jpg",
+    name: "My Heart",
+    artist: "Different Heaven & EH!DE",
+    music: "music/Different Heaven & EH!DE - My Heart.mp3",
+  },
+  {
+    img: "Superhero.jpg",
+    name: "Superhero (feat. Chris Linton)",
+    artist: "Unknown Brain",
+    music: "music/Unknown Brain - Superhero (feat. Chris Linton).mp3",
+  },
+];
+
+loadMusic(music_index);
+
+function loadMusic(music_index) {
+  clearInterval(updateTimer);
+  reset();
+
+  this_music.src = music_list[music_index].music;
+  this_music.load();
+
+  music_pic.style.backgroundImage =
+    "url(./pic/" + music_list[music_index].img + ")";
+  music_name.textContent = music_list[music_index].name;
+  music_artist.textContent = music_list[music_index].artist;
+  now_playing.textContent =
+    "Playing music " + (music_index + 1) + " of " + music_list.length;
+
+  updateTimer = setInterval(setUpdate, 1000);
+
+  this_music.addEventListener("ended", nextMusic);
+}
+
+function reset() {
+  current_time.textContent = "00:00";
+  total_time.textContent = "00:00";
+  music_slider.value = 0;
+}
+
+function playStopMusic() {
+  if (isPlaying == true) {
+    stopMusic();
+  } else {
+    playMusic();
+  }
+}
+function playMusic() {
+  this_music.play();
+  isPlaying = true;
+  music_pic.classList.add("rotate");
+  playStop_btn.innerHTML = '<i class="fa fa-pause fa-3x"></i>';
+}
+function stopMusic() {
+  this_music.pause();
+  isPlaying = false;
+  music_pic.classList.remove("rotate");
+  playStop_btn.innerHTML = '<i class="fa fa-play fa-3x"></i>';
+}
+function nextMusic() {
+  if (music_index < music_list.length - 1) {
+    music_index += 1;
+  } else {
+    music_index = 0;
+  }
+  loadMusic(music_index);
+  playMusic();
+}
+function preMusic() {
+  if (music_index > 0) {
+    music_index -= 1;
+  } else {
+    music_index = music_list.length - 1;
+  }
+  loadMusic(music_index);
+  playMusic();
+}
+function seekTo() {
+  let temp = this_music.duration * (music_slider.value / 100);
+  this_music.currentTime = temp;
+}
+function setVolume() {
+  this_music.volume = volume_slider.value / 100;
+}
+
+function setUpdate() {
+  let seekPosition = 0;
+  if (!isNaN(this_music.duration)) {
+    seekPosition = this_music.currentTime * (100 / this_music.duration);
+    music_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(this_music.currentTime / 60);
+    let currentSeconds = Math.floor(
+      this_music.currentTime - currentMinutes * 60
+    );
+    let durationMinutes = Math.floor(this_music.duration / 60);
+    let durationSeconds = Math.floor(
+      this_music.duration - durationMinutes * 60
+    );
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+
+    current_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_time.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
